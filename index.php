066fd9047dedf3a5e6ff9f0aca12959e894115d5
sourav1162 <63467856+sourav1162@users.noreply.github.com>
2020-04-12 15:17:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Studymate | Astro Developer Community</title>
+	<?php include 'links/link.php'; ?>
+	<link rel="stylesheet" type="text/css" href="css/style_index.css">
+</head>
+<body>
+<!--**************************navbar & front image start***************************-->
+
+	<div class="header" id="topheader">
+
+		<nav class="navbar navbar-expand-lg">
+			<div class="container text-uppercase p-2">
+		  		<a class="navbar-brand font-weight-bold text-white" href="#">StudyMate</a>
+		  		<button class="navbar-toggler bg-d" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		    		<span class="navbar-toggler-icon"></span>
+		  		</button>
+			  	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			    	<ul class="navbar-nav ml-auto font-weight-bold">
+			      		<li class="nav-item"><a href="#" class="nav-link text-white">Home</a></li>
+			      		<li class="nav-item"><a href="#" class="nav-link text-white">Features</a></li>
+			      		<li class="nav-item"><a href="#" class="nav-link text-white">Services</a></li>
+			      		<li class="nav-item"><a href="#" class="nav-link text-white">Pricing</a></li>
+			      		<li class="nav-item"><a href="#" class="nav-link text-white">Contact us</a></li>
+			    	</ul>
+			  	</div>
+			</div>
+		</nav>
+
+		<section class="header-section">
+			<div class="center-div">
+				<h1>We are the <span id="type-wr"></span></h1>
+				<p>Your satisfaction is our pleasure</p>
+				<div class="header-buttons">
+					<a href="">About us</a>
+					<a href="">Contact us</a>
+				</div>
+			</div>
+		</section>
+
+	</div>
+
+<!--*************************navbar & front image end******************************-->
+
+<!-- ***************************Features part start******************************* -->
+	
+	<div class="features">
+		<div class="container">
+			<div class="row">
+				<div class="feat-col col-lg-4 col-md-4 col-12">
+					<a href="#"><i class="fa-3x fa fa-shopping-cart"></i></a>
+					<h2>EASY TO ORDER</h2>
+					<p> On the Insert tab, the galleries include items that are designed to coordinate with the overall look of your document. You can use these galleries to insert tables, headers, footers, lists, cover pages, and other document building blocks.  </p>
+				</div>
+				<div class="feat-col col-lg-4 col-md-4 col-12">
+					<a href="#"><i class="fa-3x fa fa-trophy"></i></a>
+					<h2>AWESOME DESIGN</h2>
+					<p> On the Insert tab, the galleries include items that are designed to coordinate with the overall look of your document. You can use these galleries to insert tables, headers, footers, lists, cover pages, and other document building blocks.  </p>
+				</div>
+				<div class="feat-col col-lg-4 col-md-4 col-12">
+					<a href="#"><i class="fa-3x fa fa-magic"></i></a>
+					<h2>EASY TO CUSTOMIZE</h2>
+					<p> On the Insert tab, the galleries include items that are designed to coordinate with the overall look of your document. You can use these galleries to insert tables, headers, footers, lists, cover pages, and other document building blocks.  </p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<!-- ***************************Features part start******************************* -->
+
+<!-- ****************************Sevices part start******************************* -->
+	
+	<section class="services" id="servicesdiv">
+		<div class="container heading text-center">
+			<h1 class="text-center font-weight-bold text-uppercase">What do we offer</h1>
+			<p>When you create pictures, charts</p>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-12 offset-lg-0 col-10 offset-2">
+					<div class="names my-3">
+						<h1>HTML</h1>
+						<div class="progress w-75">
+							<div class="progress-bar progress-bar-animated progress-bar-striped progress-bar- bg-success" style="width: 100%;">100%</div>
+						</div>
+					</div>
+					<div class="names my-3">
+						<h1>CSS</h1>
+						<div class="progress w-75">
+							<div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" style="width: 60%;">60%</div>
+						</div>
+					</div>
+					<div class="names my-3">
+						<h1>JQUERY</h1>
+						<div class="progress w-75">
+							<div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" style="width: 50%;">50%</div>
+						</div>
+					</div>
+					<div class="names my-3">
+						<h1>REACTJS</h1>
+						<div class="progress w-75">
+							<div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width: 20%;">20%</div>
+						</div>
+					</div>
+					<div class="names my-3">
+						<h1>NODEJS</h1>
+						<div class="progress w-75">
+							<div class="progress-bar progress-bar-striped progress-bar-animated bg-info" style="width: 30%;">30%</div>
+						</div>
+					</div>
+					<div class="names my-3">
+						<h1>PHP</h1>
+						<div class="progress w-75">
+							<div class="progress-bar progress-bar-striped progress-bar-animated bg-dark" style="width: 80%;">80%</div>
+						</div>
+					</div>					
+				</div>
+
+				<div class="col-lg-6 col-md-12 offset-lg-0 col-12 ser-div">
+					<div class="row">
+						<div class="col-lg-2 col-2 col-md-2 ser-icons">
+							<i class="fa-3x fa fa-handshake-o"></i>
+						</div>
+						<div class="col-lg-10 col-md-10 col-10">
+							<h2>Website Development</h2>
+							<p>When you create pictures, charts, or diagrams, they also coordinate with your current document look</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-2 col-2 col-md-2 ser-icons">
+							<i class="fa-5x fa fa-mobile"></i>
+						</div>
+						<div class="col-lg-10 col-md-10 col-10">
+							<h2>Android Development</h2>
+							<p>When you create pictures, charts, or diagrams, they also coordinate with your current document look</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-2 col-2 col-md-2 ser-icons">
+							<i class="fa-3x fa fa-desktop"></i>
+						</div>
+						<div class="col-lg-10 col-md-10 col-10">
+							<h2>Desktop Development</h2>
+							<p>When you create pictures, charts, or diagrams, they also coordinate with your current document look</p>
+						</div>
+					</div>
+				</div>
+			</div>			
+		</div>
+	</section>
+
+<!-- ****************************Sevices part start********************************** -->
+
+
+<!-- ************************poject done part start********************************** -->
+
+	<section class="project-work">
+		<div class="container text-center">
+			<h1 class="text-center font-weight-bold text-uppercase mb-5">More than 2000+ websites are grow every hour</h1>
+			<hr class="w-100 mx-auto">
+		</div>
+
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-6">
+					<h1 class="count mt-4">1500</h1>
+					<p>CMS INSTALLATION</p>
+				</div>
+				<div class="col-lg-3 col-md-6 col-6">
+					<h1 class="count mt-4">2300</h1>
+					<p>E-COMMERCE</p>
+				</div>
+				<div class="col-lg-3 col-md-6 col-6">
+					<h1 class="count mt-4">500</h1>
+					<p>STATIC WBEBPAGE</p>
+				</div>
+				<div class="col-lg-3 col-md-6 col-6">
+					<h1 class="count mt-4">700</h1>
+					<p>DYNAMIC WBEBPAGE</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<!-- *************************poject done part end*********************************** -->
+
+
+<!-- ***************************pricing part start*********************************** -->
+
+	<section class="pricing" id="pricingdiv">
+		<div class="container p-heading text-center">
+			<h1 class="text-center font-weight-bold text-uppercase">our best price offer</h1>
+			<hr class="w-50 mx-auto">
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-4 col-12">
+					<div class="card text-center">
+						<div class="card-header">BASIC</div>
+						<div class="card-body">
+							<li>$<span class="money">20</span>/website</li>
+							<li>Responsive website</li>
+							<li>Domain name free</li>
+							<li>Webmail Supported</li>
+							<li>Customer support 24/7</li>
+						</div>
+						<div class="card-footer">
+							<a href="">Purchase</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-12 card-second">
+					<div class="card text-center">
+						<div class="card-header">RECOMENDED</div>
+						<div class="card-body">
+							<li>$<span class="line-through">70</span><span class="money">40</span>/website</li>
+							<li>Responsive website</li>
+							<li>Domain name free</li>
+							<li>Webmail Supported</li>
+							<li>Customer support 24/7</li>
+						</div>
+						<div class="card-footer">
+							<a href="">Purchase</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-12">
+					<div class="card text-center">
+						<div class="card-header">STANDARD</div>
+						<div class="card-body">
+							<li>$<span class="money">60</span>/website</li>
+							<li>Responsive website</li>
+							<li>Domain name free</li>
+							<li>Webmail Supported</li>
+							<li>Customer support 24/7</li>
+						</div>
+						<div class="card-footer">
+							<a href="">Purchase</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</section>
+
+<!-- ****************************pricing part end*********************************** -->
+
+
+<!-- ***************************happy customer part start**************************** -->
+
+
+
+<!-- ****************************happy customer part end***************************** -->
+
+
+
+
+
+<!-- ****************************jquery part start*********************************** -->
+
+	<script>
+		jQuery(document).ready(function($) {
+			$('.count').counterUp({
+				time:1000
+			});	
+		});
+	</script>
+
+	<script>
+		new Typewriter('#type-wr', {
+  			strings: ['Developer'],
+  			autoStart: true,
+  			loop:true,
+  			delay:200,
+  			deleteSpeed:200,
+  			cursor:'_'
+		});
+	</script>
+
+<!-- *****************************jquery part end************************************ -->
+</body>
+</html>
